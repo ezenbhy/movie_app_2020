98d9506d68437c46604d3b3c1147138af817fb34
ezenbhy <ezenbhy@gmail.com>
2020-10-23 12:44:31 +0900

    Updates

---
+++ precache-manifest.c4a281e8fe9f53e14b84c9cf58fd29bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6de38008d95ee6853c8edf7719ad58f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "02039111339bd3662645",
+    "url": "/movie_app_2020/static/css/main.a91fb8d6.chunk.css"
+  },
+  {
+    "revision": "ed5e0f8b7b187bd81b02",
+    "url": "/movie_app_2020/static/js/2.848afe6d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2020/static/js/2.848afe6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02039111339bd3662645",
+    "url": "/movie_app_2020/static/js/main.d5ab8888.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
